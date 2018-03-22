764b0ab7451adb8eebd0d90547e1f05a7356c296
MattHoneycutt <mbhoneycutt@gmail.com>
2018-03-22 16:47:00 -0500

    Final tweaks!

--- HeroicSamples.AspNetCore21PreviewWithAngular/Startup.cs
+++ HeroicSamples.AspNetCore21PreviewWithAngular/Startup.cs
@@ -54,14 +54,9 @@ namespace HeroicSamples.AspNetCore21PreviewWithAngular
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
