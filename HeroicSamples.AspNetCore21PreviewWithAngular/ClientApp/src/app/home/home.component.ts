42c871543683e2057b0aa5e1a401764a56aa2988
MattHoneycutt <mbhoneycutt@gmail.com>
2018-05-18 15:23:50 -0500

    Upgraded to Angular 6.

--- HeroicSamples.AspNetCore21PreviewWithAngular/ClientApp/src/app/home/home.component.ts
+++ HeroicSamples.AspNetCore21PreviewWithAngular/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  version: string;
+
+  constructor() {
+    this.version = VERSION.full;
+  }
 }
